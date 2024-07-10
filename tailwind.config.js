006b2ec602608be1a460ad003e69043c9c534c7d
jimueltomas87 <oodc.programmer4@oodc.com.ph>
2024-07-10 16:03:00 +0800

    vite to react script

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
-// /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
