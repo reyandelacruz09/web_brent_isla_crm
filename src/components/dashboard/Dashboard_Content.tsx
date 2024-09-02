7ba259947a9e9d78461ff811589bdda6bb934eb9
OODC_JONAS <jonas.bausing@oodc.com.ph>
2024-09-02 11:21:53 +0800

    dashboard images

--- src/components/dashboard/Dashboard_Content.tsx
+++ src/components/dashboard/Dashboard_Content.tsx
@@ -7,10 +7,19 @@ const Dashboard_Content = () => {
   );
   return (
     <>
-      <div className="">
+      <div className="bg-slate-100">
         <div className="w-full pt-10 flex justify-center">
-          <div className=" w-4/6">
-            <Typography variant="h5">Inventory Summary</Typography>
+          <div className=" w-5/6">
+            <div>
+              <Typography variant="h5">
+                <b>DASHBOARD</b>
+              </Typography>
+            </div>
+          </div>
+        </div>
+        <div className="w-full pt-2 flex justify-center">
+          <div className=" w-5/6 bg-white rounded-lg p-5">
+            <Typography variant="h6">Inventory Summary</Typography>
             <div className="flex pt-3">
               <div className="p-2">
                 <img src="../../images/dashboard/1/1.png" />
@@ -42,17 +51,17 @@ const Dashboard_Content = () => {
           </div>
         </div>
 
-        <div className="w-full pt-10 flex justify-center">
-          <div className=" w-4/6">
+        <div className="w-full pt-5 flex justify-center">
+          <div className=" w-5/6 ">
             <div className="">
-              <div className="flex pt-3">
-                <div className="p-2">
+              <div className="flex gap-3">
+                <div className="">
                   <img src="../../images/dashboard/2/1.png" />
                 </div>
-                <div className="p-2">
+                <div className="">
                   <img src="../../images/dashboard/2/2.png" />
                 </div>
-                <div className="p-2">
+                <div className="">
                   <img src="../../images/dashboard/2/3.png" />
                 </div>
               </div>
@@ -60,14 +69,23 @@ const Dashboard_Content = () => {
           </div>
         </div>
 
-        <div className="w-full pt-10 flex justify-center">
-          <div className=" w-4/6">
+        <div className="w-full flex justify-center">
+          <div className=" w-5/6">
             <div className="">
-              <div className="flex pt-3">
-                <div className="p-2">
+              <div className="flex pt-3 gap-3">
+                <div className="">
                   <img src="../../images/dashboard/3/1.png" />
                 </div>
-                <div className="p-2">
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="w-full flex justify-center pb-10">
+          <div className=" w-5/6">
+            <div className="">
+              <div className="flex pt-3 gap-3">
+                <div className="">
                   <img src="../../images/dashboard/3/2.png" />
                 </div>
               </div>
